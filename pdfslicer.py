31af0caba8f328e306110a88d648f9fde453c6fe
Tiago Gala <tiago.gala@gmail.com>
2015-01-24 17:24:05 +0000

    Added list of paper sizes. Procedure to calculate how many pages can we fit, may need some fine tuning

--- pdfslicer.py
+++ pdfslicer.py
@@ -3,6 +3,7 @@
 import sys
 import os.path
 import slicermodules as sm
+import math
 
 # RUN THIS:
 #	 gs  -sDEVICE=pdfwrite -o marked.pdf -c "[/CropBox [54 54 1314 810] /PAGES pdfmark" -f original.pdf
@@ -23,6 +24,7 @@ else:
 	orientation='l'
 	papersize='a4'
 	output="output.pdf"
+	original_size=(0,0)
 
 	#process args
 	sys.argv.pop(0)		#first arg is the script name, it's not useful
@@ -30,6 +32,8 @@ else:
 	#check if file exists
 	if not os.path.isfile(input_file):
 		sys.exit("Error: Bad input file: "+input_file)
+	# get page size
+	original_size = sm.getsize(input_file, page)
 
 	#processing args:
 	while len(sys.argv) >= 1:
@@ -45,7 +49,8 @@ else:
 				sys.exit("Error: No page number specified, -p switch can be ommited if the target is the first page only.")
 
 		if arg=="-s":	#FIXME: if other than page 1, this switch must be placed after -p 
-			print("Page size:", sm.getsize(input_file, page))
+			print("Width:", original_size[0], "\tHeight:", original_size[1] )
+			sys.exit(0)
 		
 		if arg=="-o":
 			if len(sys.argv) >= 1:
@@ -72,5 +77,28 @@ else:
 	# DEBUG
 #	print("Using the following options:",[input_file, page, output, orientation, papersize])
 
-	
+# CONVERT ORIENTATION TO A MORE CONVENIENT FORMAT
+if orientation=='p':
+	orientation=0
+if orientation=='l':
+	orientation=1
+
+
+#define available paper sizes	
+aformat_size = {'a0':(2384,3371),
+		'a1':(1685,2384),
+		'a2':(1190,1684),
+		'a3':( 842,1190),
+		'a4':( 595, 842),
+		'a5':( 420, 595)}
+
+print("Width:", original_size[0], "\tHeight:", original_size[1] )
+print("Target output size:", papersize, "\tOrientation:", orientation)
+
+#calculate page distribution
+pages_xx = math.ceil(original_size[0]/aformat_size[papersize][(orientation+0)&0x01])
+pages_yy = math.ceil(original_size[1]/aformat_size[papersize][(orientation+1)&0x01])
+
+print("Number of pages on the xx axis:", pages_xx)
+print("Number of pages on the yy axis:", pages_yy)
 
